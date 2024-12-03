944087d3a9d16c8cb9bab6fb5e142cf7ea457d98
Nitesh Bhardwaj <nitesh.bhardwaj@eviden.com>
2024-12-03 21:57:53 +0530

    added code for interface static methods

--- src/main/java/java_8/DefaultStaticMethods.java
+++ src/main/java/java_8/DefaultStaticMethods.java
@@ -3,9 +3,11 @@ package java_8;
 /*
 Java 8 allows interfaces to have methods with default implementations.
 This feature helps in adding new methods to interfaces without affecting the implementing classes.
+
+Interfaces can have static methods, which can be called without an instance of the interface.
  */
 
-public interface DefaultMethods {
+public interface DefaultStaticMethods {
 
     default void show(){
         System.out.println("Default Method in Interface");
@@ -13,5 +15,9 @@ public interface DefaultMethods {
 
     default void showAgain() {
         System.out.println("As many we want default method in interface");
+    }
+
+    static void staticMethod() {
+        System.out.println("Static method in Interface");
     }
 }
