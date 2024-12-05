646ecd2af7904b2ba19ad9973e078bafce62b86e
Nitesh Bhardwaj <nitesh.bhardwaj@eviden.com>
2024-12-05 18:08:20 +0530

    check largest and smallest number program

--- src/main/java/programs_top_10/FibonacciSeries.java
+++ src/main/java/programs_top_10/FibonacciSeries.java
@@ -1,4 +1,35 @@
 package programs_top_10;
 
 public class FibonacciSeries {
+    public static void main(String[] args) {
+
+        // Program to print Fibonacci Series till provided number
+        // series: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ...
+
+        int number = 10;
+
+        int first = 0;
+        int second = 1;
+
+        System.out.print("0 1 ");
+        for (int i = 1; i <= number-2; i++){
+            int sum = first + second;
+            System.out.print(sum + " ");
+            first = second;
+            second = sum;
+        }
+
+        System.out.println();
+
+        int n = 10;  // Number of terms
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a+b;
+            a = b;
+            b = next;
+        }
+    }
 }
--- src/main/java/programs_top_10/LargestSmallestNumber.java
+++ src/main/java/programs_top_10/LargestSmallestNumber.java
@@ -1,4 +1,21 @@
 package programs_top_10;
 
 public class LargestSmallestNumber {
+    public static void main(String[] args) {
+        int[] numbers = {12, 45, 3, 23, 34, 99, 22};
+
+        int largest = numbers[0];
+        int smallest = numbers[0];
+
+        for (int number : numbers){
+            if(largest < number){
+                largest = number;
+            }
+            else if(smallest > number){
+                smallest = number;
+            }
+        }
+        System.out.println("largest number: " + largest);
+        System.out.println("smallest number: " + smallest);
+    }
 }
