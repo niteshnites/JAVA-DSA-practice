bfa45d7544ee0e1e0b22b458cd7e7bbf759bf286
Nitesh Bhardwaj <nitesh.bhardwaj@eviden.com>
2024-12-05 13:22:33 +0530

    added Palindrome check program in top 10 java basic programs

--- src/main/java/programs_top_10/PalindromeCheck.java
+++ src/main/java/programs_top_10/PalindromeCheck.java
@@ -1,4 +1,34 @@
 package programs_top_10;
 
 public class PalindromeCheck {
+    public static void main(String[] args) {
+
+        // Palindrome Number String check
+        // input Naman, NITIN, madam, non -> output true
+        // input nick, nitish, man -> output false
+
+        String name = "Non";
+        boolean result = false;
+        name = name.toLowerCase();
+        for (int i = 0; i < name.length()/2; i++){
+            result = name.charAt(i) == name.charAt((name.length()-1) - i);
+            if(!result){
+                break;
+            }
+        }
+        if(result) {
+            System.out.println("Palindrome");
+        }else {
+            System.out.println("Not Palindrome");
+        }
+
+        // Other way
+        String string = "Nitesh";
+        String reversedString = new StringBuilder(string).reverse().toString();
+        if(string.equalsIgnoreCase(reversedString)) {
+            System.out.println("Palindrome");
+        }else {
+            System.out.println("Not Palindrome");
+        }
+    }
 }
---
+++ src/test/java/FactorialNumber.java
@@ -0,0 +1,20 @@
+public class FactorialNumber {
+    public static void main(String[] args) {
+
+        // factorial of a number
+        // input is 5 than output should be 120
+        // which means 1*2*3*4*5
+
+        // step 1 initialize the input and result
+        int number = 5;
+        int result = 1;
+
+        // step 2 write a loop to calculate 1 to provided number for result
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+
+        // step 3 print the result as an output
+        System.out.println("Output: " + result);
+    }
+}
