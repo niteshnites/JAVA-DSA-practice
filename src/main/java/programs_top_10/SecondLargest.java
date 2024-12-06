dbea00225aa9ef3165b0adc0f7a267a56cb6044f
Nitesh Bhardwaj <niteshnites3@gmail.com>
2024-12-06 11:27:55 +0530

    program to find the largest and second largest number

--- src/main/java/programs_top_10/SecondLargest.java
+++ src/main/java/programs_top_10/SecondLargest.java
@@ -1,4 +1,23 @@
 package programs_top_10;
 
 public class SecondLargest {
+    public static void main(String[] args) {
+
+        int [] numbers = {12, 45, 23, 67, 34, 89, 22};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int number : numbers){
+            if(number > largest){
+                secondLargest = largest;
+                largest = number;
+
+            }else if(number > secondLargest && number != largest){
+                secondLargest = number;
+            }
+        }
+        System.out.println(largest);
+        System.out.println(secondLargest);
+
+    }
 }
