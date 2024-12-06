6fcd3a51c58b3f793c1a3ac88a94dbedc7d1b221
Nitesh Bhardwaj <niteshnites3@gmail.com>
2024-12-06 11:35:15 +0530

    added count occurrence program

--- src/main/java/programs_top_10/CountOccurrences.java
+++ src/main/java/programs_top_10/CountOccurrences.java
@@ -1,4 +1,16 @@
 package programs_top_10;
 
 public class CountOccurrences {
+    public static void main(String[] args) {
+
+        String str = "programming";
+        char character = 'g';
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == character){
+                count ++;
+            }
+        }
+        System.out.println(character + " count is: " + count);
+    }
 }
