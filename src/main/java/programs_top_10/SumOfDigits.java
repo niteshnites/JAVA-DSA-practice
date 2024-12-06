54d7d7111e43837dc1020c7772971b1eea1f617f
Nitesh Bhardwaj <niteshnites3@gmail.com>
2024-12-06 11:04:52 +0530

    added sum of digits program

--- src/main/java/programs_top_10/SumOfDigits.java
+++ src/main/java/programs_top_10/SumOfDigits.java
@@ -1,4 +1,15 @@
 package programs_top_10;
 
 public class SumOfDigits {
+    public static void main(String[] args) {
+        int number = 123456789;
+        int sum = 0;
+
+        while (number != 0) {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+        System.out.println("Sum of Digits are: " + sum);
+
+    }
 }
