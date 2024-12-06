ee1404ff7c61157ba54c33036f7e24875ab085d9
Nitesh Bhardwaj <niteshnites3@gmail.com>
2024-12-06 11:14:58 +0530

    added pattern program

--- src/main/java/programs_top_10/PrintPattern.java
+++ src/main/java/programs_top_10/PrintPattern.java
@@ -1,4 +1,22 @@
 package programs_top_10;
 
 public class PrintPattern {
+    public static void main(String[] args) {
+        int n = 5;  // Number of rows
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");  // Spaces
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");  // Stars
+            }
+            System.out.println();
+        }
+    }
 }
+// output
+//    *
+//   ***
+//  *****
+// *******
+//*********
