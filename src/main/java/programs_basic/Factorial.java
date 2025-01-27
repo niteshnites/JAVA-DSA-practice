0900a67b7072138bf005554423e76e1985a3aac4
Nitesh Bhardwaj <niteshnites3@gmail.com>
2025-01-27 23:05:56 +0530

    added factorial program

---
+++ src/main/java/programs_basic/Factorial.java
@@ -0,0 +1,20 @@
+package programs_basic;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to calculate its factorial: ");
+        int number = scanner.nextInt();
+
+        long factorial = 1;  // Using long to handle larger factorial values
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("The factorial of " + number + " is: " + factorial);
+    }
+}
+
