d42ab3625bb930537206eb28af30da39a8bcdeea
niteshnites <niteshnites3@gmail.com>
2025-03-29 21:06:16 +0530

    added demot test

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,24 +1,41 @@
+import java.util.List;
+import java.util.Optional;
+
 public class Demo {
     public static void main(String[] args) {
 
         int number = 113;
         boolean isPrime = true;
 
-        if(number <= 1){
+        if (number <= 1) {
             isPrime = false;
         } else {
             for (int i = 2; i <= Math.sqrt(number); i++) {
-                if(number % i == 0){
+                if (number % i == 0) {
                     isPrime = false;
                     break;
                 }
             }
         }
 
-        if(isPrime){
+        if (isPrime) {
             System.out.println(number + " is a Prime Number");
-        }else {
+        } else {
             System.out.println(number + " is NOT a Prime Number");
         }
+    }
+}
+class DemoTest1 {
+    public static void main(String[] args) {
+        Optional<List<Integer>> id = Optional.of(List.of(1, 2, 3)); // Example data
+        String value;
+
+        if (id.isPresent()) {
+            value = id.get().contains(3) ? "fail" : "pass";
+        } else {
+            value = "trust";
+        }
+
+        System.out.println(value); // Output: "fail"
     }
 }
