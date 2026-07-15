da40eb85e34faf8f1decb9cb76fdea69328d07b3
niteshnites <niteshnites3@gmail.com>
2026-07-15 20:30:42 +0530

    added reverse

--- src/test/java/interview_prep/ReverseOfString.java
+++ src/test/java/interview_prep/ReverseOfString.java
@@ -2,11 +2,19 @@ package interview_prep;
 
 public class ReverseOfString {
     public static void main(String[] args) {
+
+        // 1.
         String strName = "Nitesh";
         String reverse = "";
         for(int i = strName.length()-1; i >= 0 ; i--) {
             reverse = reverse + strName.charAt(i);
         }
         System.out.println(reverse);
+        
+        // 2.
+        String strName2 = "Aarju";
+        StringBuffer reverse2 = new StringBuffer(strName2);
+        System.out.println(reverse2.reverse());
+
     }
 }
