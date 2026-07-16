d9f37f0f3f15dee789dcf81ab3d7b9fddb592050
niteshnites <niteshnites3@gmail.com>
2026-07-16 15:36:22 +0530

    updated

---
+++ src/test/java/interview_prep/CountCharacterFrequency.java
@@ -0,0 +1,29 @@
+package interview_prep;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CountCharacterFrequency {
+    public static void main(String[] args) {
+
+        String str = "I love programming I love programming";
+        char [] chars = str.toCharArray();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < chars.length; i++) {
+            map.put(chars[i], map.getOrDefault(chars[i], 0) + 1);
+        }
+
+        System.out.println(map);
+
+        String [] words = str.split(" ");
+        Map<String, Integer> frequencyMap = new LinkedHashMap<>();
+        for (String word : words) {
+            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+        }
+        System.out.println(frequencyMap);
+
+    }
+}
